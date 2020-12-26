5132b43de20e93a6d574b51c12cbb89954a1b5a7
Gumunista <gufelicio@outlook.com>
2020-12-26 18:29:11 -0300

    teste

---
+++ src/nomequetuquiser.js
@@ -0,0 +1,20 @@
+function juntarNomes(){
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+let nomeCompleto = input1.value + ' ' + input2.value;    
+alert(nomeCompleto);
+    
+}
+function somar(){
+let input3 = Number( document.getElementById('input3').value);
+let input4 = Number( document.getElementById('input4').value);
+let resultado =  `${input3 + input4}`
+alert(resultado);
+
+
+}
+function cursed(){
+    window.open('https://i.redd.it/cx6410i0rj751.jpg', '_blank', 'top=500,left=500,width=400,height=400, toolbar=no,scrollbars=no,resizable=no' );
+}
+  let brinco =  document.querySelector('button#oi')      
+  brinco.addEventListener('click',cursed)
